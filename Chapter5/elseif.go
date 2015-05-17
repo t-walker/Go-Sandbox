891330bc0ec67a0699565e5db26aa8b3a414f51c
Tyler Walker <twalker@eecs.wsu.edu>
2015-05-17 14:43:50 -0700

    Adding all of Chapter 5

---
+++ Chapter5/elseif.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 3
+	if i == 0 {
+			fmt.Println("Zero")
+	} else if i == 1 {
+			fmt.Println("One")
+	} else if i == 2 {
+			fmt.Println("Two")
+	} else if i == 3 {
+			fmt.Println("Three")
+	}
+}
---
+++ Chapter5/ifstatement.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	i := 5
+
+	if i % 2 == 0 {
+		fmt.Println("It's even")
+	} else {
+		fmt.Println("It's odd")
+	}
+}
